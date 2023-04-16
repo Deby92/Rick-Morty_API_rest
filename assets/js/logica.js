f781797d16d07a7e715c7c20c2e602e8440666a8
Debora Cantuarias <d.cantuarias.urrutia@gmail.com>
2023-04-15 23:44:14 -0400

    agregando cards jquery

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -42,10 +42,31 @@ function tarjeta(data) {
 
 }
 
+function mostrarImagenes() {
+    let img = document.getElementById('galeria');
+    document.getElementById('tabla_principal').style.display = 'none';
+    document.getElementById('carta').style.display = 'none';
+    document.getElementById('galeria').style.display = 'block';
+
+    img.innerHTML = "";
+    for (let temp of dataImagenes.results) {
+        img.innerHTML += `
+            <div id="card" class="card">
+            <img src="${temp.image}" class="card-img-top" alt="imagen ${temp.name}"></img>
+            <div class="card-body">
+                <h6 class="card-title">${temp.name}</h6>
+                <p class="card-text">${temp.species}</p>
+            </div>
+            </div>
+`
+    }
+}
+
 function capturaDato() {
     let nombrePersonaje = document.getElementById('dato').value;
     nombrePersonaje = nombrePersonaje.toLowerCase();
     document.getElementById('tabla_principal').style.display = 'none';
+    document.getElementById('galeria').style.display = 'none';
 
     fetch(URL_CHARACTERS + '?name=' + nombrePersonaje)
         .then(response => response.json())
@@ -64,7 +85,7 @@ $(document).ready(function () {
         .then(datos => {
             console.log(datos);
             tabla(datos.results);
+            dataImagenes = datos;
         });
 
 });
-
